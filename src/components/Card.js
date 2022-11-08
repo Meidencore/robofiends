41f009653d2543dd9b9db82d5990914a7ec3f749
Meidencore <danieleverdale@gmail.com>
2022-11-07 21:18:42 -0500

    change components class declaration to function declaration and use hooks for the container

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Card.css"
 
-class Card extends Component {
-    render() {
-        return(
-            <div className='div1 grow'>
-                <img alt="robot" src={`https://robohash.org/${this.props.id}?bgset=bg2&size=200x200`} />
-                <div>
-                    <h2>{this.props.name}</h2>
-                    <p>{this.props.email}</p>
-                </div>
+const Card = (props) => {   
+    return(
+        <div className='div1 grow'>
+            <img alt="robot" src={`https://robohash.org/${props.id}?bgset=bg2&size=200x200`} />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default Card;
